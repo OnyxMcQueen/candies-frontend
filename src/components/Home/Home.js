e3c47f83b2c3098c1505593838a3903cfd9c18e7
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-08 12:54:07 -0400

    Slight changes to paragraph text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
       <div className='hero-image'>
         <h1 className='header' style={{paddingTop: "100px"}}>Welcome to CandyLand! 🍬</h1>
         <div style={{textAlign: "center", marginLeft: "390px"}}>
-          <p style={{color: "#000000", width: "60%", fontSize: "20px", paddingTop: "20px"}}> Indulge in a sweet and delightful experience at Candy World, your ultimate destination for all things candy! Discover a delightful array of popular candies from around the world, each with its own unique flavor and charm.</p>
+          <p style={{color: "#000000", width: "60%", fontSize: "20px", paddingTop: "20px"}}> Indulge in a sweet and delightful experience at Candy Land, your ultimate destination for all things candy! Discover a delightful array of popular candies from around the world, each with its own unique flavor and charm.</p>
         </div>
         <div style={{textAlign: "center", marginTop: "110px"}}>
           <Link to='/candies/new'><button>Personalize your candy list!</button></Link>
