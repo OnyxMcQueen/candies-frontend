5a6f115d8116f40dcae3109abe4ca1c3dee8f265
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-09 14:01:37 -0400

    Bolden the paragraph text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
       <div className='hero-image'>
         <h1 className='header' style={{paddingTop: "100px"}}>Welcome to CandyLand! 🍬</h1>
         <div style={{margin: "0px auto", width: "80%"}}>
-          <p style={{textAlign: "center", color: "#000000", fontSize: "20px", paddingTop: "20px"}}> Indulge in a sweet and delightful experience at Candy Land, your ultimate destination for all things candy! Discover a delightful array of popular candies from around the world, each with its own unique flavor and charm.</p>
+          <p style={{textAlign: "center", color: "#000000", fontSize: "20px", paddingTop: "20px", fontWeight: "bolder"}}> Indulge in a sweet and delightful experience at Candy Land, your ultimate destination for all things candy! Discover a delightful array of popular candies from around the world, each with its own unique flavor and charm.</p>
         </div>
         <div style={{textAlign: "center", marginTop: "110px"}}>
           <Link to='/candies/new'><button>Personalize your candy list!</button></Link>
